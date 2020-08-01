21e01631981b4d27c67b86891ac8a275c8450b88
razered <bhagwat.work@gmail.com>
2020-08-02 00:26:46 +0530

    Minor fix in deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,17 @@
 env -i git stash 
 env -i git checkout develop
 stack exec myblog clean
-echo 'Building...'
+echo $'\nBuilding...'
 stack exec myblog build
 env -i git fetch --all
 env -i git checkout -b master --track origin/master
 echo 'Syncing...'
 rsync -a --filter='P _site/' --filter='P _cache/' --filter='P .git/' --filter='P .gitignore' --filter='P .stack-work' --delete-excluded _site/ .
-echo 'Publishing...'
+echo $'\nPublishing...'
 env -i git add -A
-env -i git commit -m 'Publish.'
+env -i git commit -m 'Publish last commit'
 env -i git push origin master:master
-echo 'Successfully published. Switching back to development branch'
+echo $'\nSuccessfully published. Switching back to development branch'
 env -i git checkout develop
 env -i git branch -D master
 env -i git stash pop
