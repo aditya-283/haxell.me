7c90af72f4379e284cb19be94b021603505dc42e
razered <bhagwat.work@gmail.com>
2020-08-02 13:44:08 +0530

    Change deployment script for CNAME

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ stack exec myblog build
 env -i git fetch --all
 env -i git checkout -b master --track origin/master
 echo 'Syncing...'
+touch _site/CNAME && echo "haxell.me" >> _site/CNAME
 rsync -a --filter='P _site/' --filter='P _cache/' --filter='P .git/' --filter='P .gitignore' --filter='P .stack-work' --delete-excluded _site/ .
 echo $'\nPublishing...'
 env -i git add -A
