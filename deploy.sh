1b8bfe903d9c8a92458d66b985e94e85584a2f8f
razered <bhagwat.work@gmail.com>
2020-08-02 15:21:40 +0530

    Change deployment

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,11 @@ env -i git fetch --all
 env -i git checkout -b master --track origin/master
 echo 'Syncing...'
 touch _site/CNAME && echo "haxell.me" >> _site/CNAME
-cp README.markdown _site/README.md
 rsync -a --filter='P _site/' --filter='P _cache/' --filter='P .git/' --filter='P .gitignore' --filter='P .stack-work' --delete-excluded _site/ .
 echo $'\nPublishing...'
 env -i git add -A
 env -i git commit -m 'Publish last commit'
-env -i git push origin master:master
+env -i git push origin master
 echo $'\nSuccessfully published. Switching back to development branch'
 env -i git checkout develop
-env -i git branch -D master
 env -i git stash pop
