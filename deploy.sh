ff38a16543112ffbe150510e6e6d0a381e966226
razered <bhagwat.work@gmail.com>
2020-08-02 15:37:40 +0530

    Change deployment

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,13 @@ env -i git checkout develop
 stack exec myblog clean
 echo $'\nBuilding...'
 stack exec myblog build
-env -i git checkout master
+git checkout master
 echo 'Syncing...'
-touch _site/CNAME && echo "haxell.me" >> _site/CNAME
 rsync -a --filter='P _site/' --filter='P _cache/' --filter='P .git/' --filter='P .gitignore' --filter='P .stack-work' --delete-excluded _site/ .
 echo $'\nPublishing...'
-env -i git add -A
-env -i git commit -m 'Publish last commit'
-env -i git push origin master
+git add -A
+git commit -m 'Publish last commit'
+git push origin master
 echo $'\nSuccessfully published. Switching back to development branch'
-env -i git checkout develop
+git checkout develop
 env -i git stash pop
